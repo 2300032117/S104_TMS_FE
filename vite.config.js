c6883299d148fba5ff92f39b3ed78f4e67b84281
2300032117 <163526961+2300032117@users.noreply.github.com>
2025-10-18 10:29:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/S104_TMS_FE/', // MUST match your repo name
   plugins: [react()],
-   base: '/S104_TMS_FE/'
-})
+});
